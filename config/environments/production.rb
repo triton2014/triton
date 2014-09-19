5137b1d597b0d5c721c72ccad4f3b86c99e33243
priyankodey <priyankodey87@gmail.com>
2014-09-19 15:36:14 +0530

    mailer added

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -86,4 +86,13 @@ config.action_mailer.perform_deliveries = true
 config.action_mailer.raise_delivery_errors = false
 config.action_mailer.default :charset => "utf-8"
 
+config.action_mailer.smtp_settings = {
+  address:              'smtp.gmail.com',
+  port:                 587,
+  domain:               "gmail.com",
+  user_name:            'priyanko.dey33@gmail.com',
+  password:              ENV['SMTP_PASSWORD'],
+  authentication:       'plain',
+  enable_starttls_auto: true  }
+
 end
